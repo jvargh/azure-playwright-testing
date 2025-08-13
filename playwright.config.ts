4183a08c1154918d171d7d3147049d26a78b58ea
Joji Varghese <joji_varghese@hotmail.com>
2025-08-12 23:25:16 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-  baseURL: process.env.BASE_URL || 'https://nodewebapp1-asa6dyfec6hvcdc9.centralus-01.azurewebsites.net',
+  baseURL: process.env.BASE_URL || 'https://<webapp-name>.centralus-01.azurewebsites.net',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'retain-on-failure',
